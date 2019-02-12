5f9cd69af99ee15cb4f8187228cf5d2b5ccffa78
Zachary Walters <zswalter@oakland.edu>
2019-02-11 23:06:20 -0500

    Print Beer Song

--- BeerSong.java
+++ BeerSong.java
@@ -3,5 +3,10 @@ public class BeerSong {
     	Ninety_Nine_Bottles_of_Beer(); 
     }
     public static void Ninety_Nine_Bottles_of_Beer() {
+    	int i = 100;
+     	while (i > 0) {
+     		Sysem.out.println((i) + " bottles of beer on the wall, " + (i) + " bottles of beer take one down, pass it around, " + (i-1) +" bottles of beer on the wall.");
+     		i--;
+     	}
     }
 }
